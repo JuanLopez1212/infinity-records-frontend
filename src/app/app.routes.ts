6a3f123aba8ad2422099b0a8b7c71fe0669d3c7d
Juan D Lopez <juandiegolopezortiz4@gmail.com>
2025-06-11 18:40:23 -0500

    se añaden el resto de pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { Home } from './pages/public/home/home';
+import { Login } from './pages/public/login/login';
+import { Register } from './pages/public/register/register';
+import { Albums } from './pages/private/albums/albums';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'home', component: Home },
+    { path: 'login', component: Login },
+    { path: 'register', component: Register },
+    { path: 'albums', component: Albums },
+    { path: '**', redirectTo: 'home', pathMatch: 'full' },
+    { path: '', redirectTo: 'home', pathMatch: 'full' } 
+];
