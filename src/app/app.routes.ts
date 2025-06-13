d17ba21b5bd92b80bf612e3c111623ff585171d4
Juan Romero <juan2905sbr@gmail.com>
2025-06-12 20:48:20 -0500

    app.routes.ts:define rutas pages/: Define componentes (public,private) product.component:Define Formularii

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,12 +3,16 @@ import { Home } from './pages/public/home/home';
 import { Login } from './pages/public/login/login';
 import { Register } from './pages/public/register/register';
 import { Albums } from './pages/private/albums/albums';
+import { UserNewForm } from './pages/private/users/new-form/new-form';
+import { Users } from './pages/private/users/users';
 
 export const routes: Routes = [
     { path: 'home', component: Home },
     { path: 'login', component: Login },
     { path: 'register', component: Register },
     { path: 'albums', component: Albums },
+    {path:'dashboard/users', component:Users},
+    {path: 'dashboard/users/new', component: UserNewForm},
     { path: '**', redirectTo: 'home', pathMatch: 'full' },
     { path: '', redirectTo: 'home', pathMatch: 'full' } 
 ];
