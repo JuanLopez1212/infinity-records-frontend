a279e780f4c57fb4169ba2ce1b562f9239540cdc
Juan D Lopez <juandiegolopezortiz4@gmail.com>
2025-07-01 21:39:50 -0500

    Se validó en el navegador el login para que mostrara en la consola el token con un usuario existente en la base de datos

--- src/app/pages/public/login/login.ts
+++ src/app/pages/public/login/login.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { AuthServices } from '../../../services/auth-services';
 
 @Component({
   selector: 'app-login',
@@ -11,10 +12,10 @@ export class Login {
   // Atributos de la clase
   formData!: FormGroup
 
-  constructor () {
+  constructor ( private authService: AuthServices ) {
     // Define la agrupación de campos del formulario
     this.formData = new FormGroup({
-      username: new FormControl ( '', [ Validators.required, Validators.email ] ),
+      email: new FormControl ( '', [ Validators.required, Validators.email ] ),
       password: new FormControl ( '', [ Validators.required, Validators.minLength ( 6 ), Validators.maxLength ( 12 )] )
     });
   }
@@ -22,6 +23,18 @@ export class Login {
   onSubmit () {
     if ( this.formData.valid ) {
       console.log ( this.formData.value )
+
+      this.authService.loginUser ( this.formData.value ).subscribe({
+        next: ( data ) => {
+          console.log ( data )
+        },
+        error: ( error ) => {
+          console.log ( error )
+        },
+        complete: () => {
+          this.formData.reset()
+        }
+      }) 
     }
   }
 }
