8988cd4559dc8cca9b97a9210490ecc36b4000c7
Juan Romero <juan2905sbr@gmail.com>
2025-07-01 21:40:20 -0500

    se completo el login user y se valido en el navegador para que mostrara en la consola con un usuario que ya existia en la base de datos

--- src/app/pages/public/login/login.ts
+++ src/app/pages/public/login/login.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { AuthServices } from '../../../services/auth-services';
 
 @Component({
   selector: 'app-login',
@@ -11,16 +12,28 @@ export class Login {
 
   formData!: FormGroup
 
-  constructor(){
+  constructor(private authService: AuthServices){
     this.formData = new FormGroup ({
-      username: new FormControl('', [Validators.required, Validators.email]),
+      email: new FormControl('', [Validators.required, Validators.email]),
       password: new FormControl('', [Validators.required, Validators.minLength(6),
         Validators.maxLength(12)])
     });
     }
     onsubmit () {
       if(this.formData.valid){
-        console.log(this.formData.value)
+        console.log(this.formData.value);
+
+        this.authService.loginUser( this.formData.value).subscribe({
+          next: (data) => {
+            console.log(data);
+          },
+          error:(error) => {
+            console.error(error);
+          },
+          complete:  () => {
+            this.formData.reset();
+          }
+        });
       }
   }
 }
