4ff62051478319b527b0e5163837643985957bff
Juan D Lopez <juandiegolopezortiz4@gmail.com>
2025-07-01 21:06:20 -0500

    login.component: Define formulario reactivo, validando campos y mostrando sus respectivos mensajes de error

--- src/app/pages/public/login/login.ts
+++ src/app/pages/public/login/login.ts
@@ -1,11 +1,27 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [ ReactiveFormsModule ],
   templateUrl: './login.html',
   styleUrl: './login.css'
 })
 export class Login {
+  // Atributos de la clase
+  formData!: FormGroup
 
+  constructor () {
+    // Define la agrupación de campos del formulario
+    this.formData = new FormGroup({
+      username: new FormControl ( '', [ Validators.required, Validators.email ] ),
+      password: new FormControl ( '', [ Validators.required, Validators.minLength ( 6 ), Validators.maxLength ( 12 )] )
+    });
+  }
+
+  onSubmit () {
+    if ( this.formData.valid ) {
+      console.log ( this.formData.value )
+    }
+  }
 }
