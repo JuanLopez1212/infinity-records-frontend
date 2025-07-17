c19c094f9b19e2f34d58b7dcaa80dfff0db63e80
Juan D Lopez <juandiegolopezortiz4@gmail.com>
2025-07-17 16:14:18 -0500

    Se hace el diseño de login, formularios y tablas de listado

--- src/app/pages/public/artists-public/artists-public.ts
+++ src/app/pages/public/artists-public/artists-public.ts
@@ -8,54 +8,6 @@ import { Component, signal, ViewChild, ElementRef } from '@angular/core';
   styleUrl: './artists-public.css'
 })
 export class ArtistsPublic {
-
-  artistas = signal<any[]>([
-    {
-      username: 'Luar La L',
-      img: 'img/luarlal.webp',
-      genero: 'Trap'
-    },
-    {
-      username: 'Bad Bunny',
-      img: 'img/badbunny.webp',
-      genero: 'Trap'
-    },
-    {
-      username: 'Ryan Castro',
-      img: 'img/ryancastro.webp',
-      genero: 'Reggaeton'
-    },
-    {
-      username: 'DFZM',
-      img: 'img/dfzm.webp',
-      genero: 'Reggaeton'
-    },
-    {
-      username: 'Rels B',
-      img: 'img/rels-b.webp',
-      genero: 'Rap'
-    },
-    {
-      username: 'Eladio Carrion',
-      img: 'img/eladiocarrion.webp',
-      genero: 'Trap'
-    },
-    {
-      username: 'Lia Kali',
-      img: 'img/liakali.webp',
-      genero: 'Rap'
-    },
-    {
-      username: 'Duki',
-      img: 'img/duki.webp',
-      genero: 'Trap'
-    },
-    {
-      username: 'Rauw Alejandro',
-      img: 'img/rauw.webp',
-      genero: 'Reggaeton'
-    }
-  ]);
-
+  
 }
 
